1599fd6fbd0b2fdad9e5d30ebe399158d83cfdc9
Nick Budden <hello@nickbudden.com>
2013-11-16 21:50:26 -0800

    Working issue

--- app/scripts/view/editor.js
+++ app/scripts/view/editor.js
@@ -42,7 +42,21 @@ define([
 
       afterRender: function() {
         var self = this;
-        self.paper = Raphael("tshirt", 295, 230);
+        self.paper = Raphael("tshirt", 161, 194);
+
+        // self.overlay = self.paper.path("M0 0L73 0L0 72L0 0");
+        // self.overlay = self.paper.path("M 181 181 l 0.000 100.000 l -100.000 0.000 l 0.000 -100.000 l 100.000 0.000");
+
+        // self.overlay.attr("fill", "rgb(9,10,42)");
+        // self.overlay.attr("fill", "#06071C");
+        // self.overlay.attr("stroke", "#06071C");
+        // self.overlay.attr("opacity", ".8");
+
+        // self.overlay2 = self.paper.path("M0 100L53 112L0 72L0 0");
+        // self.overlay2.attr("fill", "http://static.tumblr.com/44e684098f0ac7c33a6640c20556b923/jxahzkb/fZ5mod2rw/tumblr_static_dog-logo.jpg");
+
+
+        console.log(self.overlay);
       },
 
       addPocket: function( img_url ) {
@@ -69,6 +83,9 @@ define([
         self.imageTransform.setOpts({
           scale: false
         });
+
+        // temporary
+        self.overlay.toFront();
       }
 
     });
